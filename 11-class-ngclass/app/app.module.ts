fb5a6f3cdc022a49c3e8adba3ad78f17c8985367
Avjol Sakaj <avjolsakaj@outlook.com>
2019-03-12 09:58:15 +0100

    Done before services

--- 11-class-ngclass/app/app.module.ts
+++ 11-class-ngclass/app/app.module.ts
@@ -1,17 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 
-import { AppComponent } from './app.component';
+import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CommonModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, PassengerDashboardModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
