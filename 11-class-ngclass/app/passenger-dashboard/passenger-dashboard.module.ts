1c278f928c8ba121f839703380e3c9dccc88de66
Avjol Sakaj <avjolsakaj@outlook.com>
2019-03-13 16:06:55 +0100

    Finishing forms

--- 11-class-ngclass/app/passenger-dashboard/passenger-dashboard.module.ts
+++ 11-class-ngclass/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
 
 // Containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerViewerComponent } from "./containers/passenger-viewer/passenger-viewer.component";
 
 // Components
 import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
 import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
+import { PassengerFormComponent } from "./components/passenger-form/passenger-form.component";
 
 //Service
 import { PassengerDashboardService } from "./passenger-dashboard.service";
 
 @NgModule({
   declarations: [
+    // Containers
     PassengerDashboardComponent,
+    PassengerViewerComponent,
+
+    //Generic Components
     PassengerCountComponent,
-    PassengerDetailComponent
+    PassengerDetailComponent,
+    PassengerFormComponent
   ],
-  imports: [CommonModule, HttpModule],
-  exports: [PassengerDashboardComponent],
+  imports: [CommonModule, HttpModule, FormsModule],
+  exports: [PassengerViewerComponent],
   providers: [PassengerDashboardService]
 })
 export class PassengerDashboardModule {}
